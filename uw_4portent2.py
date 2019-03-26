e2079f3232e059278610323e7f18fef14cc14867
boiledcodeandbroccoli <44714801+boiledcodeandbroccoli@users.noreply.github.com>
2019-03-25 17:26:42 -0700

    Update uw_4portent2.py

--- uw_4portent2.py
+++ uw_4portent2.py
@@ -44,8 +44,8 @@ print('Amount of 4 land openers:                                    \t' + str(ro
 print('Amount of times we hit ANY BLUE SOURCE + 1 cantrip:          \t' + str(round(count_blue / num_iterations,3))) # need to check logic in the loop
 print('Amount of ZERO land openers:                                 \t' + str(round(count_no_lands / num_iterations,3)))
 print('Amount of ZERO cantrip opener:                               \t' + str(round(count_no_cantrips / num_iterations,3)))
-print('One cantrip given 1 valid blue source in hand:               \t' + str(round(cantrip_only / lands_only,3))) # this seems wrong. valid blue source is defined as keepable in the dark, i.e, no Tundras allowed.
-print('One cantrip given 1 of ANY blue source in hand:              \t' + str(round(cantrip_only / count_all_lands,3))) # cantrip per any blue source available to the deck.
+print('One cantrip given 1 valid blue source in hand:               \t' + str(round(cantrip_only / lands_only,3))) # Seems weird/wrong. "Valid blue source" is defined as keepable in the dark, i.e, no Tundras allowed. We only want to count Basic Island or things that are equivalent to Basic Island (blue fetches only).
+print('One cantrip given 1 of ANY blue source in hand:              \t' + str(round(cantrip_only / count_all_lands,3))) # Seems weird. cantrip per any blue source available to the deck.
 
 #debugging
 #print('Testing print of iterations  - only islands                      \t' + str(round(count_island_only,3)))

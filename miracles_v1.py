0f1cb870343e5609696ccaa2afd40ed237669aea
boiledcodeandbroccoli <44714801+boiledcodeandbroccoli@users.noreply.github.com>
2019-03-26 09:58:07 -0700

    Update miracles_v1.py

--- miracles_v1.py
+++ miracles_v1.py
@@ -8,15 +8,15 @@ This is my paper build
 '''
 
 
-num_iterations = 100000
-cards_seen = 7
+num_iterations = 100000 # Number of times we want to do this
+cards_seen = 7 # Always = 7 for an opening hand.
 
-decklist = {'Plains': 2, 'Islands': 6, 'Blue_Fetches': 8, 'Arid_Mesa': 1, 'Tundra': 2, 'Cantrips':11, 'Others': 30} #we take out karakas in this build and replace with +1 cantrip. Also -1 spell +1 cantrip for the Full Cartel of 12 Cantrips.
+decklist = {'Plains': 2, 'Islands': 6, 'Blue_Fetches': 8, 'Arid_Mesa': 1, 'Tundra': 2, 'Cantrips':11, 'Others': 30} # Creating the decklist
 deck = []
 for card in decklist.keys():
     deck += [card] * decklist[card]
 
-#simulation loop
+# Simulation loops
 
 count_island_cantrip = 0 #counts how many times we have BASIC ISLANDS + 1 cantrip in our hand.
 count_flood = 0 #counts how many times we have 4+ landers i.e, flooding
@@ -30,36 +30,36 @@ count_island_only = 0 #for debugging purposes
 
 
 for _ in range(num_iterations):
-    draw = Counter(random.sample(deck, cards_seen))
+    draw = Counter(random.sample(deck, cards_seen)) 
+    
+''' 
+draw function tells Python to randomly select 7 cards from the decklist, and count how many types of cards are in it.
+random.sample(deck, cards_seen) takes a random saple of 'cards_seen' (the opening 7 card hand) from our created decklist.
+The random 7 card sample gets fed into the 'Counter' function, which then gives a dictionary that is used to look up how many
+of each card was in the given sample.
+'''     
     count_island_cantrip += (min(draw['Islands'], 1) + min(draw['Cantrips'], 1) >= 2) # minimum of 1 Basic Island AND at least 1 Cantrip
     count_flood += (draw['Islands'] + draw['Blue_Fetches'] + draw['Plains'] + draw['Arid_Mesa'] + draw['Tundra'] >= 4) # drawing a 4 lander in the 7
-    count_blue += (min(draw['Islands'], 1) + min(draw['Cantrips'], 1) + min(draw['Blue_Fetches'], 1) >=2) # this calc is probably wrong. minimum of 1 BLUE SOURCE (i.e, island + duals + fetches) AND 1 cantrip
     count_no_lands += (draw['Islands'] + draw['Blue_Fetches'] + draw['Arid_Mesa'] + draw['Tundra'] == 0) # the good ol' no lander
     count_no_cantrips += (draw['Cantrips'] == 0) # use this to calculate zero cantrips in the opener
     cantrip_only += (min(draw['Cantrips'], 1) >= 1) #drawing at least 1 cantrip
     good_lands_only += (min(draw['Islands'], 1) + min(draw['Blue_Fetches'], 1) >= 1) # minimum of having at least 1 land in your opener but no Tundra + no Mesa
     count_all_lands += (min(draw['Islands'], 1) + min(draw['Blue_Fetches'], 1) + min(draw['Arid_Mesa'], 1) + min(draw['Tundra'], 1) >=1) #trying to use this to tally up all blue sources with a minimum of 1 appearance in the hand
-    count_island_only += (draw['Islands'] >= 1)
+    count_island_only += (draw['Islands'] >= 1) 
 
 
-#printing results
+# Printing results
 
 for card, number in decklist.items():
     print(card + ': ' + str(number) + '\t', end='')
 print()
 print('Amount of times we hit BASIC ISLAND + 1 cantrip:                 \t' + str(round(count_island_cantrip / num_iterations,3)))
 print('Amount of 4 land openers:                                        \t' + str(round(count_flood / num_iterations,3)))
-print('Amount of times we hit Non-Wastable blue source + 1 cantrip:     \t' + str(round((cantrip_only / num_iterations) * (good_lands_only / num_iterations) ,3))) # need to check logic in the loop
-print('Amount of times we hit any blue source + 1 cantrip:              \t' + str(round((cantrip_only / num_iterations) * (count_all_lands / num_iterations) ,3))) # need to check logic in the loop
+print('Amount of times we hit Non-Wastable blue source + 1 cantrip:     \t' + str(round((cantrip_only / num_iterations) * (good_lands_only / num_iterations) ,3))) 
+print('Amount of times we hit any blue source + 1 cantrip:              \t' + str(round((cantrip_only / num_iterations) * (count_all_lands / num_iterations) ,3))) 
 print('Amount of ZERO land openers:                                     \t' + str(round(count_no_lands / num_iterations,3)))
 print('Amount of ZERO cantrip opener:                                   \t' + str(round(count_no_cantrips / num_iterations,3)))
-#print('One cantrip given 1 valid blue source in hand:                   \t' + str(round(cantrip_only / lands_only, 3))) # this seems wrong. valid blue source is defined as keepable in the dark, i.e, no Tundras allowed.
-#print('One cantrip given 1 of ANY blue source in hand:                  \t' + str(round(cantrip_only / count_all_lands, 3))) # this also seems wrong.
 
-#debugging
+#debugging stuff
 #print('Testing print of iterations  - only islands                      \t' + str(round(count_island_only,3)))
 #print('Testing print of iterations  - only cantrips                     \t' + str(round(cantrip_only,3)))
-#print('Testing print of cantrips/islands only                           \t' + str(round(cantrip_only/count_island_only,3)))
-#print('Testing print of iterations  - all lands                         \t' + str(round(count_all_lands,3)))
-#print('Testing print of iterations  - zero cantrips                     \t' + str(round(count_no_cantrips,3)))
-#print('Testing print of iterations  - basic island + 1 cantrip           \t' + str(round(count_island_cantrip,3)))
